73850b586c07d9327f4f35bd74778ba039889220
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2017-10-06 16:41:35 +0200

    remove imports

--- HashBreak/src/main/java/org/funsec/util/Permutations.java
+++ HashBreak/src/main/java/org/funsec/util/Permutations.java
@@ -1,10 +1,5 @@
 package org.funsec.util;
 
-import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 public class Permutations {
