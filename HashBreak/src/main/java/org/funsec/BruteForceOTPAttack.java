6294328f9291d2806cd30768449332802fa38622
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2017-10-10 16:43:37 +0200

    update parallel code

--- HashBreak/src/main/java/org/funsec/BruteForceOTPAttack.java
+++ HashBreak/src/main/java/org/funsec/BruteForceOTPAttack.java
@@ -1,15 +1,17 @@
 package org.funsec;
 
 import org.funsec.hmac.HOTP;
-import org.funsec.util.Utils;
 import org.funsec.util.Permutations;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Consumer;
+import java.util.function.ObjIntConsumer;
+import java.util.function.ObjLongConsumer;
 
 import static org.funsec.OTPToken.matchWithStolen;
 import static org.funsec.OTPToken.printOtpValues;
@@ -18,18 +20,231 @@ import static org.funsec.util.Utils.asCharStr;
 /**
  * Performs a brute force attack on random generated keys which simulate stolen tokens.<br/>
  * <p/>
- *
  */
 public class BruteForceOTPAttack {
 
 
+    //32^4 = 1048576
+
     public static void main(String arg[]) throws InterruptedException {
-        runAttack(8, GoogleTOTP.ENCODE_TABLE, HOTP.SHA_1);
+
+        ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()-2);
+
+        int keyLen = 16;
+
+        OTPToken[] stolenTokens = OTPToken.stealOTP(keyLen, GoogleTOTP.ENCODE_TABLE, 10, HOTP.SHA_1);
+
+        OTPToken firstToken = stolenTokens[0];
+
+        System.out.println("Selected random key: " + Arrays.toString(firstToken.getKey()) + " ( " + asCharStr(firstToken.getKey()) + ") ==> " + firstToken.getOtp());
+
+
+        int otp = firstToken.getOtp();
+
+        AtomicLong counter = new AtomicLong();
+        AtomicLong matchCounter = new AtomicLong();
+
+        List<byte[]> matchedKeys = new ArrayList<>();
+
+        long start = System.currentTimeMillis();
+
+        ObjIntConsumer<byte[]> handler = (byte[] bts, int count) -> {
+
+
+            if (count % 1000000 == 0 && count != 0) {
+                counter.lazySet(count);
+                System.err.println("Processed: " + count + " in " + (System.currentTimeMillis() - start) + "ms");
+            }
+
+
+            if (matchWithStolen(stolenTokens, bts, HOTP.SHA_1)) {
+
+                matchedKeys.add(Arrays.copyOf(bts, bts.length));
+                System.out.println("Found key match: " + Arrays.toString(bts) + " (" + asCharStr(bts) + ") ==> " + otp);
+                matchCounter.incrementAndGet();
+
+                throw new RuntimeException();
+            }
+
+        };
+
+        StringBuilder buff = new StringBuilder();
+        for(int i = 0;i  < 32; i++) {
+            for(int a = 0; a < 32; a++) {
+                for(int b = 0; b < 32; b++) {
+                    for(int c = 0; c < 32; c++) {
+                        for(int d = 0; d < 32; d++) {
+                            if(buff.length() != 0)
+                                buff.append(',');
+
+                            buff.append(i + ":" + a + ":" + b + ":" + c + ":" + d + ":");
+                        }
+                    }
+                }
+            }
+        }
+
+//        System.out.println(buff.toString());
+//        if(true) return;
+
+        splitAttack(exec, handler,keyLen, GoogleTOTP.ENCODE_TABLE, HOTP.SHA_1, buff.toString());
+
+        exec.shutdown();
+        exec.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+
+//        Map<String, Object> opts = TOTPApp.argumentsAsMap(arg);
+//
+//        int keyLen = 0;
+//        if (opts.get("keylen") == null) {
+//            printHelp();
+//            System.exit(-1);
+//        } else {
+//            keyLen = Integer.parseInt(opts.get("keylen").toString());
+//        }
+//
+//        byte[] alphabet = GoogleTOTP.ENCODE_TABLE;
+//
+//        if (opts.get("alphabet") != null) {
+//            alphabet = opts.get("alphabet").toString().getBytes();
+//        }
+//
+//        if (opts.get("splitinfo") != null) {
+//            Permutations.printKeySpaceSplit(keyLen, GoogleTOTP.ENCODE_TABLE);
+//        } else if (opts.get("alphabetsplitinfo") != null) {
+//            Permutations.printAlphabetInfoSegment(keyLen, alphabet);
+//        } else if (opts.get("alphabetinfo") != null) {
+//            Permutations.printAlphabetInfo(keyLen, alphabet);
+//        } else if (opts.get("run") != null) {
+//            if (opts.get("splits") == null) {
+//                runAttack(keyLen, alphabet, HOTP.SHA_1);
+//            } else {
+//                String splits = opts.get("splits").toString();
+//
+//                splitAttack(exec, keyLen, alphabet, HOTP.SHA_1, splits);
+//            }
+//        } else {
+//            printHelp();
+//            System.exit(-1);
+//        }
+//
+//        System.exit(0);
     }
 
-    public static void runAttack(int keyLen, byte[] alphabet, String shaAlgo) {
+    private static void printHelp() {
+        System.out.println("-keylen <keylength> -alphabet <bytestring -splits <number of splits> e.g ABC> <command>");
+        System.out.println("commands: splitinfo, alphabetsplitinfo, alphabetinfo, run");
+    }
 
-        long totalPermutations = (long)Math.pow(alphabet.length, keyLen);
+    /**
+     * Is part of a split attack simulation.
+     * <p>
+     * e.g
+     * <pre>
+     * indexes = 0:0,0:1,1:0,1:1 // the size of indexes is the total number of splits assigned to run on this node, here is is 4.
+     *                           // note that the node can execute columns concurrently
+     *                           // x1:x2:x3 form a path down the splits
+     *                           // x1 => segment in sp1, x2 segment in sp2 etc..
+     *                           //
+     *                           // So a single path is a single segment coordinate down the splits to the last split.
+     *                           //
+     * splits = [ sp1, sp2 ]     // split path, sp1 is split into sp2 and so on.
+     *
+     * sp1.segments = 2, segment_len=200
+     * sp2.segments = 2, segment_len = 50
+     *
+     * For 0:0 sp1.segments[0], sp2.segments[0],
+     * For 0:1 sp1.segments[0], sp2.segments[1],
+     * For 1:0 sp1.segments[1], sp2.segments[0],
+     * For 1:1 sp1.segments[1], sp2.segments[1],
+     * </pre>
+     */
+    private static void splitAttack(ExecutorService exec,
+                                    ObjIntConsumer<byte[]> handler,
+                                    int keyLen, byte[] alphabet, String shaAlgo, String splitpath) {
+
+        List<Permutations.SegmentIndexPath> indexes = Permutations.SegmentIndexPath.parsePath(splitpath);
+
+        List<Permutations.Split> splits = Permutations.splitKeySpace(keyLen, alphabet);
+
+        byte[] word = new byte[splits.get(0).keyLen];
+
+        System.out.println("Splits: " + splits);
+        System.out.println("Indexes: " + indexes);
+
+        for (Permutations.SegmentIndexPath coord : indexes) {
+//            exec.submit(() -> {
+//                try {
+                    runSplitAttackBuild(exec, handler, word, 0, splits, coord, shaAlgo);
+//                } catch (Throwable t) {
+//                    t.printStackTrace();
+//                }
+//            });
+        }
+    }
+
+    /**
+     * Recursively go through each level of the split building the word constants for the attack.
+     * When the lowest level is reached, the attack is started.
+     *
+     * @param word
+     * @param lvl
+     * @param splits
+     * @param coord
+     */
+    private static void runSplitAttackBuild(ExecutorService exec,
+                                            ObjIntConsumer<byte[]> handler,
+                                            byte[] word, int lvl, List<Permutations.Split> splits, Permutations.SegmentIndexPath coord, String shaAlgo) {
+
+
+        Permutations.Split sp = splits.get(lvl);
+        updateWord(word, lvl, sp, coord);
+
+        if(coord.children.size() > 0) {
+
+            System.out.println("coordsize: > 0 :" + coord);
+
+            for(Permutations.SegmentIndexPath childCoord : coord.children) {
+                runSplitAttackBuild(exec, handler, word, lvl+1, splits, childCoord, shaAlgo);
+            }
+        } else {
+                runSplitAttackExec(exec, handler, word, lvl, sp, shaAlgo);
+        }
+    }
+
+    private static void runSplitAttackExec(ExecutorService exec,
+                                           ObjIntConsumer<byte[]> handler,
+                                           byte[] word, final int lvl, Permutations.Split sp, String shaAlgo) {
+        System.out.println("Work: " + Arrays.toString(word));
+        for(int i = 0; i < sp.getColumnCount(); i++) {
+
+            final byte[] columnWord = Arrays.copyOf(word, word.length);
+            columnWord[columnWord.length-1] = sp.getKEndConstant(i);
+
+            exec.submit(() -> {
+
+                Permutations.addAndCarry(columnWord, sp.keyLen-1, lvl, GoogleTOTP.ENCODE_TABLE, handler);
+
+            });
+        }
+
+    }
+
+    private static void updateWord(byte[] word, int lvl, Permutations.Split sp, Permutations.SegmentIndexPath coord) {
+        word[lvl] = sp.getSegmentConstant(coord.index);
+    }
+
+
+    /**
+     * Run a single threaded attack simulation
+     *
+     * @param keyLen
+     * @param alphabet
+     * @param shaAlgo
+     * @throws InterruptedException
+     */
+    public static void runAttack(int keyLen, byte[] alphabet, String shaAlgo) throws InterruptedException {
+
+        long totalPermutations = (long) Math.pow(alphabet.length, keyLen);
 
         long start = System.currentTimeMillis();
 
@@ -48,14 +263,13 @@ public class BruteForceOTPAttack {
 
         List<byte[]> matchedKeys = new ArrayList<>();
 
-        Consumer<byte[]> handler = (byte[] bts) -> {
+        ObjIntConsumer<byte[]> handler = (byte[] bts, int count) -> {
 
-            long count = counter.getAndIncrement();
 
-            if( count % 1000000 == 0 && count != 0) {
-                System.err.println("Processed: " + count + " of " + totalPermutations);
+            if (count % 1000000 == 0 && count != 0) {
+                counter.lazySet(count);
+                System.err.println("Processed: " + count + " of " + totalPermutations + " " + (System.currentTimeMillis() - start) + "ms");
             }
-
 
 
             if (matchWithStolen(stolenTokens, bts, shaAlgo)) {
@@ -73,7 +287,7 @@ public class BruteForceOTPAttack {
 
         long end = System.currentTimeMillis();
 
-        System.out.println("Searched " + counter.get() + " " + ((int)(((double)counter.get()/totalPermutations)*100)) + "% permutations in " + (end - start) + "ms found " + matchCounter.get());
+        System.out.println("Searched " + counter.get() + " " + ((int) (((double) counter.get() / totalPermutations) * 100)) + "% permutations in " + (end - start) + "ms found " + matchCounter.get());
 
         System.out.println("Printing match correlations: ");
 
@@ -89,7 +303,6 @@ public class BruteForceOTPAttack {
         System.out.println("--------------------------------------------------------------------");
 
     }
-
 
 
 }
--- HashBreak/src/main/java/org/funsec/util/Permutations.java
+++ HashBreak/src/main/java/org/funsec/util/Permutations.java
@@ -1,23 +1,190 @@
 package org.funsec.util;
 
-import java.util.function.Consumer;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntFunction;
+import java.util.function.ObjIntConsumer;
 
 public class Permutations {
+
+    public static void main(String arg[]) throws InterruptedException {
+//        printAlphabetInfo(4, "ABC".getBytes());
+        printAlphabetInfoSegment(3, "ABC".getBytes());
+
+        for(Split split : splitKeySpace(4, "ABC".getBytes())){
+            System.out.println(split);
+        }
+
+/*
+         keyLen:  4
+         Split{keyLen=4, alphabet=3, total=81, rows=27, segment_len=9, segments=3}
+         Split{keyLen=2, alphabet=3, total=9, rows=3, segment_len=1, segments=3}
+
+         */
+    }
+
+    public static final void printKeySpaceSplit(int keyLen, byte[] alphabet) {
+        for(Split split : splitKeySpace(keyLen, alphabet)){
+            System.out.println(split);
+        }
+
+    }
+
+    /**
+     * Print out all permutations for a key from an alphabet.<br/>
+     * Total = alphabet_len^keyLen
+     * Rows = total/alphabet_len
+     * Column[i].k_end = alphabet[i]   => column 0, the last element in the tuple is a constant from alphabet[0] and so on.
+     * <p>
+     * Segments:
+     */
+    public static final void printAlphabetInfo(int keyLen, byte[] alphabet) {
+
+
+        IntFunction<ObjIntConsumer<byte[]>> printer = (n) -> (bts, len) -> {
+            int x = len % n;
+
+            System.out.print("\t" + Utils.asCharStr(bts) + (x == 0 ? '\n' : ""));
+
+        };
+
+
+        int alphabetLen = alphabet.length;
+        addAndCarry(keyLen, alphabet, printer.apply(alphabetLen));
+
+        int total = (int) Math.pow(3, 4);
+        int rows = (total / alphabetLen);
+        int segment_len = rows / alphabetLen;
+        int segments = rows / segment_len;
+
+        System.out.println("total " + total + " rows: " + rows + " cols " + alphabetLen);
+
+        for (int i = 0; i < alphabetLen; i++) {
+            System.out.print("Column[" + i + "].K_end =>  " + ((char) alphabet[i]) + " ");
+        }
+        System.out.println();
+
+        System.out.println("Segment_len: " + segment_len + " of " + segments + " segments");
+    }
+
+    public static final void printAlphabetInfoSegment(int keyLen, byte[] alphabet) {
+        IntFunction<ObjIntConsumer<byte[]>> printer = (n) -> (bts, len) -> {
+            int x = len % n;
+
+            System.out.print("\t" + Utils.asCharStr(bts) + (x == 0 ? '\n' : ""));
+
+        };
+
+
+        Split split = new Split(keyLen, alphabet);
+
+        addAndCarry(new byte[keyLen], 0, split, printer.apply(split.getColumnCount()));
+    }
+
+
+    /**
+     *
+     * Divide the keyspace into splits where each subsequent Split instance identifies a whole rectangle of keyspace with
+     * the same amount of columns but 2 keys less.
+     *
+     * e.g
+     * <pre>
+     * AAAA	AAAB	AAAC
+     * AABA	AABB	AABC
+     * AACA	AACB	AACC
+     *
+     * ABAA	ABAB	ABAC
+     * ABBA	ABBB	ABBC
+     * ABCA	ABCB	ABCC
+     *
+     * ACAA	ACAB	ACAC
+     * ACBA	ACBB	ACBC
+     * ACCA	ACCB	ACCC
+     *
+     * BAAA	BAAB	BAAC
+     * BABA	BABB	BABC
+     * BACA	BACB	BACC
+     * BBAA	BBAB	BBAC
+     * BBBA	BBBB	BBBC
+     * BBCA	BBCB	BBCC
+     * BCAA	BCAB	BCAC
+     * BCBA	BCBB	BCBC
+     * BCCA	BCCB	BCCC
+     * CAAA	CAAB	CAAC
+     * CABA	CABB	CABC
+     * CACA	CACB	CACC
+     * CBAA	CBAB	CBAC
+     * CBBA	CBBB	CBBC
+     * CBCA	CBCB	CBCC
+     * CCAA	CCAB	CCAC
+     * CCBA	CCBB	CCBC
+     * CCCA	CCCB	CCCC
+     * </pre>
+     *
+     * splits[0] = {segments=3, segment_length=9, rows=27, word[col=0]=['A', ?, ?, 'A'],   word[col=1]=['A', ?, ?, 'B'],   word[col=2]=['A', ?, ?, 'C']}
+     * splits[1] = {segments=3, segment_length=3, rows=9,  word[col=0]=['A', 'A', ?, 'A'], word[col=1]=['A', 'A', ?, 'B'], word[col=2]=['A', 'A', ?, 'C']}
+     *
+     * This means:
+     *  each segment in split[1] is repeated splits[0].segments times and for each column where column.length == alphabet.length
+     */
+    public static final List<Split> splitKeySpace(int keyLen, byte[] alphabet, int splitLimit, int sizeLimit) {
+
+        List<Split> splits = new ArrayList<>();
+        Split split = new Split(keyLen, alphabet);
+
+        splits.add(split);
+
+
+        while (split.keyLen > 3 &&
+                (split.total.longValue() > sizeLimit) &&
+                splits.size() < splitLimit) {
+
+            split = split.split();
+            splits.add(split);
+        }
+
+        return splits;
+    }
+
+    public static final List<Split> splitKeySpace(int keyLen, byte[] alphabet) {
+        return splitKeySpace(keyLen, alphabet, 4, 10);
+    }
+
+    /**
+     * segmentIndexes are the segments from 0...segment_length in the last split that should be populated for each
+     */
+    public static void permutate(List<Split> split, int[] segmentIndexes) {
+
+    }
+
+    public static void addAndCarry(byte[] word, int wordI, Split segment, ObjIntConsumer<byte[]> handler) {
+        addAndCarry(word, segment.keyLen, wordI, segment.alphabet, handler);
+    }
+
+
+    public static void addAndCarry(int len, byte[] alphabet, ObjIntConsumer<byte[]> handler) {
+        addAndCarry(new byte[len], len, 0, alphabet, handler);
+    }
 
     /**
      * Call the handler once for each permutation alphabet in len, calculated by count(alphabet)^len.
      */
-    public static void addAndCarry(int len, byte[] alphabet, Consumer<byte[]> handler) {
-        byte[] word = new byte[len];
+    public static void addAndCarry(byte[] word, int len, int wordI, byte[] alphabet, ObjIntConsumer<byte[]> handler) {
+
         int[] index = new int[len];
+        int q;
+
+        int count = 0;
 
         while (true) {
 
-            for (int i = 0; i < len; i++)
-                word[i] = alphabet[index[i]];
+            copy(len, alphabet, word, wordI, index);
+
+            count++;
 
             try {
-                handler.accept(word);
+                handler.accept(word, count);
             } catch (RuntimeException rte) {
                 return;
             }
@@ -26,13 +193,198 @@ public class Permutations {
             for (int i = len - 1; ; i--) {
                 if (i < 0) return;
 
-                index[i]++;
-                if (index[i] == alphabet.length)
+                q = index[i] + 1;
+                index[i] = q;
+
+                if (q == alphabet.length)
                     index[i] = 0;
                 else
                     break;
 
             }
+        }
+    }
+
+
+    private static final void copy(int len, byte[] alphabet, byte[] word, int wordI, int[] index) {
+        for (int i = wordI; i < len; i++)
+            word[i] = alphabet[index[i]];
+    }
+
+    /**
+     * A Segment takes advantage of how permutations can be grouped into rows and columns.
+     * Doing this give us insight into optimizing and splitting permutations into segments.
+     * Segments can be split themselves into sub-segments.
+     * <p>
+     * total = alphabet_len ^ key_len
+     * rows = total / alphabet_len
+     * segment_len = rows / alphabet_len
+     * segments = rows / segment_len
+     * <p>
+     * A segment represents N rows of columns data. The first item of a permutation tuple is constant within a segment,
+     * and the last item is constant for each column ( the number of columns is equal to the alphabet size).
+     * The permutations for each column can be calculated as if the key_len has been reduced by 2.
+     * <p>
+     * <p>
+     * e.g
+     * <pre>
+     * AAAA	AAAB	AAAC
+     * AABA	AABB	AABC
+     * AACA	AACB	AACC
+     * ABAA	ABAB	ABAC
+     * ABBA	ABBB	ABBC
+     * ABCA	ABCB	ABCC
+     * ACAA	ACAB	ACAC
+     * ACBA	ACBB	ACBC
+     * ACCA	ACCB	ACCC
+     * BAAA	BAAB	BAAC
+     * BABA	BABB	BABC
+     * BACA	BACB	BACC
+     * BBAA	BBAB	BBAC
+     * BBBA	BBBB	BBBC
+     * BBCA	BBCB	BBCC
+     * BCAA	BCAB	BCAC
+     * BCBA	BCBB	BCBC
+     * BCCA	BCCB	BCCC
+     * CAAA	CAAB	CAAC
+     * CABA	CABB	CABC
+     * CACA	CACB	CACC
+     * CBAA	CBAB	CBAC
+     * CBBA	CBBB	CBBC
+     * CBCA	CBCB	CBCC
+     * CCAA	CCAB	CCAC
+     * CCBA	CCBB	CCBC
+     * CCCA	CCCB	CCCC
+     * total 81 rows: 27 cols 3
+     * Column[0].K_end =>  A Column[1].K_end =>  B Column[2].K_end =>  C
+     * Segment_len: 9 of 3 segments
+     * </pre>
+     */
+    public static class Split {
+
+        public final int keyLen;
+        public final byte[] alphabet;
+
+        public final BigInteger total;
+        public final BigInteger rows;
+        public final BigInteger segment_len;
+        public final int segments;
+
+        public Split(int keyLen, byte[] alphabet) {
+
+            this.keyLen = keyLen;
+            this.alphabet = alphabet;
+
+            BigInteger alphabetLen = new BigInteger(String.valueOf(alphabet.length));
+
+            total = alphabetLen.pow(keyLen);
+            rows = total.divide(alphabetLen);
+            segment_len = rows.divide(alphabetLen);
+            segments = rows.divide(segment_len).intValue(); //segment_len is never larger than the alphabet len
+        }
+
+        /**
+         * The number of columns is always equal to the alphabet len
+         *
+         * @return
+         */
+        public int getColumnCount() {
+            return alphabet.length;
+        }
+
+        /**
+         * [x,x,x....K]
+         *
+         * @param col
+         * @return byte for K which is constant throughout the column
+         */
+        public byte getKEndConstant(int col) {
+            return alphabet[col];
+        }
+
+        public byte getSegmentConstant(int segmentIndex) {
+            return alphabet[segmentIndex];
+        }
+
+        public Split split() {
+
+            System.out.println("keyLen:  " + keyLen);
+            if (keyLen < 3) {
+                throw new RuntimeException("The segments cannot be split more " + keyLen);
+            }
+
+            return new Split(keyLen - 1, alphabet);
+        }
+
+        @Override
+        public String toString() {
+            return "Split{" +
+                    "keyLen=" + keyLen +
+                    ", alphabet=" + alphabet.length+
+                    ", total=" + total +
+                    ", rows=" + rows +
+                    ", segment_len=" + segment_len +
+                    ", segments=" + segments +
+                    '}';
+        }
+    }
+
+    public static final class SegmentIndexPath{
+
+        public final int index;
+        public final List<SegmentIndexPath> children;
+
+        public SegmentIndexPath(int index, String str) {
+            this(index, parsePath(str));
+        }
+
+        public SegmentIndexPath(int index) {
+            this(index, new ArrayList<>());
+        }
+
+        public SegmentIndexPath(int index, List<SegmentIndexPath> children) {
+            this.index = index;
+            this.children = children;
+        }
+
+
+        public static List<SegmentIndexPath> parsePath(String str) {
+            String[] paths = str.split(",");
+
+            List<SegmentIndexPath> arr = new ArrayList<>();
+
+            for(String path : paths) {
+
+                String[] parts = path.split(":");
+
+                if(parts.length == 1)
+                    arr.add(new SegmentIndexPath(Integer.parseInt(parts[0])));
+                else
+                    arr.add(new SegmentIndexPath(Integer.parseInt(parts[0]), join(parts, ':', 1)));
+
+            }
+
+            return arr;
+        }
+
+        private static String join(String[] parts, char ch, int from) {
+            StringBuilder buff = new StringBuilder();
+            for(int i = from; i < parts.length; i++) {
+                if(i != from)
+                    buff.append(ch);
+
+                buff.append(parts[i]);
+            }
+
+            return buff.toString();
+        }
+
+        @Override
+        public String toString() {
+            return "SegmentIndexPath{" +
+                    "index=" + index +
+                    ", children=" + children +
+                    '}';
         }
     }
 
